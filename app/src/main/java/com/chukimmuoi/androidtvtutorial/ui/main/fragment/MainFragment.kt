d6b4392c893fe845cdd0cee6eeda3f1b1a52f5e4
Chử Kim Mười (VinSmart) <v.muoick@vinsmart.net.vn>
2019-08-15 10:59:31 +0700

    Step 01

--- app/src/main/java/com/chukimmuoi/androidtvtutorial/ui/main/fragment/MainFragment.kt
+++ app/src/main/java/com/chukimmuoi/androidtvtutorial/ui/main/fragment/MainFragment.kt
@@ -1,8 +1,12 @@
 package com.chukimmuoi.androidtvtutorial.ui.main.fragment
 
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 import androidx.leanback.app.BrowseFragment
 import androidx.leanback.app.BrowseSupportFragment
+import androidx.leanback.widget.Presenter
+import androidx.leanback.widget.PresenterSelector
+import com.chukimmuoi.androidtvtutorial.R
 
 /**
  * @author  : Vinsmart
@@ -16,5 +20,23 @@ class MainFragment: BrowseSupportFragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        setupUIElements()
+    }
+
+    private fun setupUIElements() {
+        activity?.let {
+            badgeDrawable = ContextCompat.getDrawable(it, R.drawable.videos_by_google_banner)
+            title = "Videos by Google" // Badge, when set, takes precedent
+            // over title
+            headersState = HEADERS_ENABLED
+            isHeadersTransitionOnBackEnabled = true
+
+            // set fastLane (or headers) background color
+            brandColor = ContextCompat.getColor(it, R.color.fastlane_background)
+
+            // set search icon color
+            searchAffordanceColor = ContextCompat.getColor(it, R.color.search_opaque)
+        }
     }
 }
